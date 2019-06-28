29267587ecc47c415578c390e73fc98bf05d1d78
pasotee <oalexprea@yahoo.com>
2019-06-28 15:50:54 +0300

    node refactor

--- Lista/Nod.h
+++ Lista/Nod.h
@@ -4,15 +4,19 @@
 template <class T>
 class Node
 {
-	T data; //Stores a value in the node
-	Node* next; //Points toward the next node from the list
 public:
 	Node(T val);
 	~Node();
-	Node<T>* getNext() const;
-	T getData() const;
-	void setNext(Node<T>* p );
+
+    Node<T>* getNext() const;
+    void setNext(Node<T>* p );
+	
+    T getData() const;
 	void setData(T val);
+
+protected:
+    T data; //Stores a value in the node
+    Node* next; //Points toward the next node from the list
 };
 
 #include "Nod.impl"
